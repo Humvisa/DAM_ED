abce4289bbe72f4da15332a43b5c7c1e13420e4a
Humvisa <humberto.villegas1054@gmail.com>
2025-10-23 12:47:49 +0200

    Creacion del repositorio, primeros 2 ejercicios

---
+++ src/ejercicio1.java
@@ -0,0 +1,11 @@
+public class ejercicio1 {
+    static void saludar() {
+        System.out.println("HOLAAAAAAAAAA");
+    }
+
+    static void main() {
+        saludar();
+        saludar();
+        saludar();
+    }
+}
---
+++ src/ejercicio2.java
@@ -0,0 +1,9 @@
+public class ejercicio2 {
+    static int sumar(int num1, int num2) {
+        return num1 + num2;
+    }
+    static void main() {
+        int resultado = sumar(5, 8);
+        System.out.println(resultado);
+    }
+}
