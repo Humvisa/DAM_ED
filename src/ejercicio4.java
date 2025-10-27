5204cf24ab423d8d28ba3ce6203db6604a307bcb
Humvisa <humberto.villegas1054@gmail.com>
2025-10-27 17:14:25 +0100

    nuevos ejercicios 4 y 5

--- src/ejercicio4.java
+++ src/ejercicio4.java
@@ -1,5 +1,8 @@
 public class ejercicio4 {
     public static void main(String[] args) {
-
+        areaRectangulo (50, 70);
+    }
+    public static void areaRectangulo(int base, int altura){
+        System.out.println("el area del rectangulo es " + (base * altura));
     }
 }
---
+++ src/ejercicio5.java
@@ -0,0 +1,13 @@
+public class ejercicio5 {
+    public static void main(String[] args) {
+    parOImpar(61);
+    }
+    static void parOImpar(int num1) {
+    if (num1%2==0) {
+        System.out.println("el numero " + num1 + " es par");
+    }
+    else {
+        System.out.println("el numero " + num1 + " es impar");
+    }
+    }
+}
