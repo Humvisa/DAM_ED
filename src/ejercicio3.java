39d3788d9ad46b118b53f4deab7e0ad5fb096428
Humvisa <humberto.villegas1054@gmail.com>
2025-10-27 16:58:47 +0100

    nuevos ejercicios

---
+++ src/ejercicio3.java
@@ -0,0 +1,9 @@
+public class ejercicio3 {
+    static void main() {
+        saludar("juan", 16);
+    }
+    static void saludar(String nombre, int edad) {
+        System.out.println("hola " + nombre +" con años " + edad);
+
+    }
+}
