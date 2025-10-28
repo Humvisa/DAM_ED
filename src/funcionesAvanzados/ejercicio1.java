aca1fbf3bbf291f5c48e9c6a99d300f6603ec00c
Humvisa <humberto.villegas1054@gmail.com>
2025-10-28 12:20:28 +0100

    ejercicio 1 terminao (un infierno)

--- src/funcionesAvanzados/ejercicio1.java
+++ src/funcionesAvanzados/ejercicio1.java
@@ -2,16 +2,18 @@ package funcionesAvanzados;
 //Implementa un método recursivo que determine si un número es primo.
 public class ejercicio1 {
     public static void main(String[] args) {
-        numeroPrimo(1003);
-    }
-    public static void numeroPrimo(int num1) {
-        int i=num1-1;
-        while (i > 1){
-            if (num1%i!=0){
-                i--;
-            }
-        }
-        System.out.println("primo");
-        }
+        numeroPrimo(3);
     }
 
+    public static void numeroPrimo(int num1) {
+        for (int i = num1-1; i > 1; i--) {
+            if (num1%i == 0) {
+                i=0;
+                System.out.println("no primo");
+                } else if (i == 2) {
+                System.out.println("primo");
+                }
+            }
+        }
+    }
+    //sin ayudas pero me tarde como 5 horas probando cosas
