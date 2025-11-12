0cce4c08d62dcd6f4cc9554ce3e76ab8bb8e17a2
Humvisa <humberto.villegas1054@gmail.com>
2025-11-12 17:00:09 +0100

    Array Ejercicios

---
+++ src/Arrays/ArraysEj5.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+//Dado un array de 50 números entre 1 y 100,
+// aleatorios, escribir una lista ordenada con los valores
+// que no aparecen en el array.
+public class ArraysEj5 {
+    public static void main(String[] args) {
+        int[] array = new int[50];
+        Random random = new Random();
+        for (int i=0; i< array.length; i++){
+            int numeroRandom = random.nextInt(100);
+            array[i] = numeroRandom;
+        }
+        int[] excluidos = new int[50];
+        for (int i = 0; i < excluidos.length; i++){
+            if (array [i] != excluidos[0]);
+        }
+        System.out.println("Array Random " + Arrays.toString(array));
+    }
+}
