699230ac652fea568f8a227b480ad27e5fca3c85
humvisa <humberto.villegas1054@gmail.com>
2026-01-16 02:35:59 +0100

    Ej 2 del tema 3 hecho

--- src/Tema3/Ej2/MainPersona.java
+++ src/Tema3/Ej2/MainPersona.java
@@ -2,7 +2,7 @@ package Tema3.Ej2;
 
 import java.util.Scanner;
 
-public class MainPersona {
+public class  MainPersona {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("dime el nombre");
@@ -13,11 +13,11 @@ public class MainPersona {
         char sexo = sc.next().charAt(0);
         System.out.println("dime el peso en kg");
         double peso = sc.nextDouble();
-        System.out.println("dime el altura en M con ,");
+        System.out.println("dime la altura en M con ,"); //con . me da error
         double altura = sc.nextDouble();
         Persona persona1 = new Persona(nombre, edad, sexo,peso,altura);
 
-        System.out.println("dime el nombre");
+        System.out.println("dime el 2do nombre");
         String nombre1 = sc.next();
         System.out.println("dime la edad");
         int edad1 = sc.nextInt();
@@ -25,28 +25,61 @@ public class MainPersona {
         char sexo1 = sc.next().charAt(0);
         Persona persona2 = new Persona(nombre1, edad1, sexo1);
 
-        System.out.println("dime el nombre");
-        String nombre2 = sc.nextLine();
+        System.out.println("dime el 3er nombre");
+        String nombre2 = sc.next();
         Persona persona3 = new Persona(nombre2);
 
+        Persona[] personas = new Persona[3];
+        personas[0] = persona1;
+        personas[1] = persona2;
+        personas[2] = persona3;
 
-        if (persona1.calcularIMC() == -1){
-            System.out.println("esta por debajo de su peso ideal");
-        } else if (persona1.calcularIMC() == 0) {
-            System.out.println("esta en su peso ideal");
-        }else {
-            System.out.println("tiene sobrepeso");
+        for (int i = 0; i < personas.length; i++){
+            if (personas[i].calcularIMC() == 2){ // para cuando no hay suficientes datos
+                System.out.println("no se pudo calcular el imc de " + personas[i].getNombre());
+            }
+            if (personas[i].calcularIMC() == -1){
+                System.out.println(personas[i].getNombre() + " esta por debajo de su peso");
+            } else if (personas[i].calcularIMC() == 0) {
+                System.out.println(personas[i].getNombre() + " esta en su peso ideal");
+            }else if (personas[i].calcularIMC() == 1){
+                System.out.println(personas[i].getNombre() + " esta en sobrepeso");
+            }
+            if (personas[i].esMayorDeEdad()){
+                System.out.println(personas[i].getNombre() + " es mayor de edad");
+            }else {
+                System.out.println(personas[i].getNombre() + " es menor de edad");
+            }
         }
-        persona2.calcularIMC();
-
-        persona3.calcularIMC();
-
-
-        persona1.esMayorDeEdad();
-        persona2.esMayorDeEdad();
-        persona3.esMayorDeEdad();
-
-        persona1.toString();
+        for (int i = 0; i < personas.length; i++){
+            System.out.println(personas[i].toString());
+        }
         sc.close();
+//Haz una clase llamada Persona que siga las siguientes condiciones:
+//Sus atributos son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura. No queremos que se accedan directamente a ellos. Piensa que modificador de visibilidad es el más adecuado, también su tipo. Si quieres añadir algún atributo puedes hacerlo.
+//Se implantarán varios constructores:
+//Un constructor con el nombre.
+//Un constructor con el nombre, edad y sexo.
+//Un constructor con todos los atributos como parámetro, salvo el DNI.
+//Los métodos que se implementarán son:
+//calcularIMC(): calculara si la persona está en su peso ideal (peso en kg/(altura^2 en m)), si esta fórmula devuelve un valor menor que 20, el método devuelve un -1, si devuelve un número entre 20 y 25 (incluidos), está en un peso normal, por tanto, devuelve un 0 y si devuelve un valor mayor que 25 significa que tiene sobrepeso, devuelve un 1. Te recomiendo que uses constantes para devolver estos valores.
+//esMayorDeEdad(): indica si es mayor de edad, devuelve un booleano.
+//comprobarSexo(char sexo): comprueba que el sexo introducido es correcto. Si no es correcto, será H. No sería visible al exterior. Devolverá el carácter correspondiente al sexo.
+//toString(): devuelve toda la información del objeto.
+//generaDNI(): genera un número aleatorio de 8 cifras y con ese número, calcula su letra correspondiente. Este método será invocado cuando se construya el objeto. No será visible al exterior.
+//Para calcular el DNI se debe obtener el módulo 23 del número. Y una vez obtenido, se relaciona el resto con la letra correspondiente: 
+//
+//
+//Métodos set y get de cada atributo.
+//Ahora, crea una clase principal que haga lo siguiente:
+//Pide por teclado el nombre, la edad, sexo, peso y altura.
+//Crea 3 objetos de la clase anterior:
+//El primer objeto utilizará todas las variables.
+//El segundo objeto utilizará todas las variables menos el peso y la altura.
+//El último utilizará el constructor que solo dispone del nombre.
+//Para cada persona se deberá comprobar si está en su peso ideal, tiene sobrepeso o por debajo de su peso ideal con un mensaje.
+//También hay que indicar, para cada persona, si es mayor de edad.
+//Por último, se debe mostrar la información de cada objeto.
+//NOTA: Deberás utilizar los métodos que consideres oportunos para realizar todas las acciones indicadas.
     }
 }
--- src/Tema3/Ej2/Persona.java
+++ src/Tema3/Ej2/Persona.java
@@ -15,17 +15,20 @@ public class Persona {
     public Persona(String nombre, int edad, char sexo) {
         this.nombre = nombre;
         this.edad = edad;
-        this.sexo = sexo;
+        this.sexo = comprobarSexo(sexo);
     }
 
     public Persona(String nombre, int edad, char sexo, double peso, double altura) {
         this.nombre = nombre;
         this.edad = edad;
-        this.sexo = sexo;
+        this.sexo = comprobarSexo(sexo); //para aplicar bien la comprobacion use chatgpt
         this.peso = peso;
         this.altura = altura;
     }
     public int calcularIMC(){
+        if (peso == 0 || altura == 0){
+            return 2;
+        }
         if (peso / (altura * altura) < 20){
             return -1;
         }if (peso / (altura * altura) >= 20 && peso / (altura * altura) <= 25){
@@ -47,9 +50,9 @@ public class Persona {
         }
     }
     public String toString(){
-        return "el nombre es: "+ nombre + " edad: "+ edad + " sexo: "+ sexo + " peso: "  + peso + " altura: "+ altura;
+        return "el nombre es: "+ nombre + " edad: "+ edad + " sexo: "+ sexo + " peso: "  + peso + " altura: "+ altura + " DNI: "+ generarDNI();
     }
-    public StringBuilder generarDNI(){
+    private StringBuilder generarDNI(){
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 8 ; i++) {
             int digito = (int) (Math.random() * 10);
